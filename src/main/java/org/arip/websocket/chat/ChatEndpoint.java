b4078460c177946c1e817266a52cb14db28e7257
Arip Hidayat <ariphidayat@hotmail.com>
2016-02-20 20:50:27 +0700

    broadcast message to all connected sessions

--- src/main/java/org/arip/websocket/chat/ChatEndpoint.java
+++ src/main/java/org/arip/websocket/chat/ChatEndpoint.java
@@ -1,6 +1,8 @@
 package org.arip.websocket.chat;
 
 import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.logging.Logger;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
@@ -13,22 +15,34 @@ import javax.websocket.server.ServerEndpoint;
 public class ChatEndpoint {
     private final Logger log = Logger.getLogger(getClass().getName());
 
+    private Session session;
+    private static final Set<ChatEndpoint> chatEndpoints = new CopyOnWriteArraySet<>();
+
     @OnOpen
     public void onOpen(Session session) throws IOException {
+        this.session = session;
+        chatEndpoints.add(this);
+
         String message = session.getId() + " connected!";
         log.info(message);
-        session.getBasicRemote().sendText(message);
+        broadcast(message);
     }
 
     @OnMessage
     public void onMessage(Session session, String message) throws IOException, EncodeException {
         log.info(message);
-        session.getBasicRemote().sendText(message);
+        broadcast(message);
     }
 
     @OnClose
     public void onClose(Session session) throws IOException {
         log.info(session.getId() + " disconnected!");
-        session.getBasicRemote().sendText(session.getId() + " disconnected!");
+        broadcast(session.getId() + " disconnected!");
+    }
+
+    private static void broadcast(String message) throws IOException {
+        for (ChatEndpoint endpoint : chatEndpoints) {
+            endpoint.session.getBasicRemote().sendText(message);
+        }
     }
 }
